7f271aec5166f10ad5deedf8abd8ef77e1576432
Olya <olapancisina1@gmail.com>
2024-01-18 13:48:49 +0100

    add components

---
+++ src/components/HashtagList.tsx
@@ -0,0 +1,11 @@
+export default function HashtagList() {
+	return (
+		<ul className='hashtags'>
+			<li><button>#ByteGrad</button></li>
+			<li><button>#Nike</button></li>
+			<li><button>#McDonalds</button></li>
+			{/* <li><button>ByteGrad</button></li>
+			<li><button>ByteGrad</button></li> */}
+		</ul>
+	)
+}
