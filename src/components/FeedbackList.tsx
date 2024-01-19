2bf93225a965a6fe9f8f54b89826fbd56d0aabba
Olya <olapancisina1@gmail.com>
2024-01-19 11:05:22 +0100

    add error message

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
+import ErrorMessage from './ErrorMessage'
 import FeedbackItem from './FeedbackItem'
 import Spinner from './Spinner'
 
 export default function FeedbackList() {
 	const [feedbackItem, setFeedbackItem] = useState([])
 	const [isLoading, setLoading] = useState(false)
+	const [errorMessage, setErrorMessage] = useState('')
 
 	useEffect(() => {
 		setLoading(true)
@@ -12,22 +14,33 @@ export default function FeedbackList() {
 			'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
 		)
 			.then(response => {
+				if (!response.ok) {
+					throw new Error()
+				}
 				return response.json()
 			})
 			.then(data => {
 				setFeedbackItem(data.feedbacks)
 				setLoading(false)
 			})
+			.catch(() => {
+				setErrorMessage('Something went wrong')
+				setLoading(false)
+			})
 	}, [])
 
 	return (
 		<ol className='feedback-list'>
-			{isLoading ? <Spinner/> : feedbackItem.map(feedbackItem => (
-				<FeedbackItem feedbackItem={feedbackItem} />
-			))
-		
-		}
-			
+			{isLoading ? (
+				<Spinner />
+			) : (
+				feedbackItem.map(feedbackItem => (
+					<FeedbackItem feedbackItem={feedbackItem} />
+				))
+			)}
+			{errorMessage ? <ErrorMessage message={errorMessage} /> : feedbackItem.map(feedbackItem => (
+					<FeedbackItem feedbackItem={feedbackItem} />
+				))}
 		</ol>
 	)
 }
