1a6ffe6a0097f55c6b06bfcc43f2f453c60037c5
Olya <olapancisina1@gmail.com>
2024-06-28 10:04:09 +0200

    fix 1.1

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,12 +1,12 @@
-import { useContext } from 'react'
-import { FeedbackItemsContext } from '../contexts/FeedbackItemsContextProvider'
+// import { useContext } from 'react'
+// import { FeedbackItemsContext } from '../contexts/FeedbackItemsContextProvider'
 
-export function useFeedbackItemsContext() {
-	const context = useContext(FeedbackItemsContext)
-	if (!context) {
-		throw new Error(
-			'FeedbackItemsContext is not defined in FeedbackList component'
-		)
-	}
-	return context
-}
+// export function useFeedbackItemsContext() {
+// 	const context = useContext(FeedbackItemsContext)
+// 	if (!context) {
+// 		throw new Error(
+// 			'FeedbackItemsContext is not defined in FeedbackList component'
+// 		)
+// 	}
+// 	return context
+// }
